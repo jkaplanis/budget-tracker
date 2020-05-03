04a4ca54c8c613efaeb11e7cbd2738c08fd9132c
jkaplanis <johntkaplanis@gmail.com>
2020-05-02 17:02:28 -0700

    add saveRecord function, checkDatabase function

--- public/db.js
+++ public/db.js
@@ -23,10 +23,49 @@ request.onerror = event => {
 // TODO: add code so that any transactions stored in the db
 // are sent to the backend if/when the user goes online
 // Hint: learn about "navigator.onLine" and the "online" window event.
+request.onsuccess = function (event) {
+  db = event.target.result;
+
+  // check if app is online before reading from db
+  if (navigator.onLine) {
+    checkDatabase();
+  }
+};
+
+function checkDatabase() {
+  const transaction = db.transaction(["pending"], "readwrite");
+  const store = transaction.objectStore("pending");
+  const getAll = store.getAll();
+
+  getAll.onsuccess = () => {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json"
+        }
+      })
+        .then(response => response.json())
+        .then(() => {
+          const transaction = db.transaction(["pending"], "readwrite");
+          const store = transaction.objectStore("pending");
+          store.clear();
+        });
+    }
+  };
+}
 
 // TODO: add code to saveRecord so that it accepts a record object for a
 // transaction and saves it in the db. This function is called in index.js
 // when the user creates a transaction while offline.
 function saveRecord(record) {
   // add your code here
+  const transaction = db.transaction(["pending", "readwrite"]);
+  const store = transaction.objectStore("pending");
+  store.add(record);
 }
+
+// listen for app coming back online
+window.addEventListener("online", checkDatabase);
