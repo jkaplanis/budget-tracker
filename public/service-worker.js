23c5dbe4755decae32b4287cf485a6475343f605
jkaplanis <johntkaplanis@gmail.com>
2020-05-02 20:36:32 -0700

    add files to cache to service worker js

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,3 +4,13 @@
 // when the app is used offline. HINT: You should use two caches. One for the
 // static assets such ass html, css, js, images, etc; and another cache for
 // the dynamic data from requests to routes beginning with "/api".
+
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/manifest.webmanifest",
+  "/icons/icon-152x152.png",
+  "/icons/icon-192x192.png",
+  "/db.js",
+  "/index.js",
+]
